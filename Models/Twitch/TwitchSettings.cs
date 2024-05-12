43950550ea1fc95e72a1b4cf112a91e7a6707caa
Mickey@MYH2 <nimiauredhel@gmail.com>
2024-05-12 15:40:42 +0300

    fixed issue with saving twitch settings

--- Models/Twitch/TwitchSettings.cs
+++ Models/Twitch/TwitchSettings.cs
@@ -6,7 +6,7 @@ public class TwitchSettings
 {
     // temporary hack to make sure sqlite doesn't save a lot of unnecessary stuff we're not using
     [Unique] 
-    public int id { get; private set; } = 0;
+    public int id { get; set; } = 1;
 
     public string Username { get; set; } = string.Empty;
     public string Token { get; set; } = string.Empty;
